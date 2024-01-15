4379ca04f41f754c0a4c5000b9e6ade65a98bf1e
kevyngabriel <kevyngabriel.pessoal@gmail.com>
2024-01-15 17:22:52 -0300

    Update script.js

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,12 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN");
 40 + 8 + 23 - 10;
-console.log(40 + 8 + 23 - 10);
+
+let firstName = "Jonas";
+let secondName = "Pereira";
+
+console.log(firstName + secondName);
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+console.log(myFirstJob);
