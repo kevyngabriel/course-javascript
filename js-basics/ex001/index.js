00976c66d68d1a556dea123635ebee7da1bbd9d1
kevyngabriel <kevyngabriel.pessoal@gmail.com>
2024-06-28 05:35:24 -0300

    js

---
+++ js-basics/ex001/index.js
@@ -0,0 +1,8 @@
+// let para variáveis
+// const para constantes
+
+let name = "Aidan"; // string
+let age = 42; // number
+let isApproved = true; //boolean
+let firstName = undefined;
+let selectedColor = null; //reset value the variable
